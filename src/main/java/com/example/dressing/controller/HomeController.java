3ad88522413821c867658c75065a2d29bfa48d17
haaa32 <125627991+haaa32@users.noreply.github.com>
2023-09-23 14:35:31 +0900

    Add files via upload

---
+++ src/main/java/com/example/dressing/controller/HomeController.java
@@ -0,0 +1,14 @@
+//대표 컨트롤러
+package com.example.dressing.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    //기본 페이지 요청 메서드
+    @GetMapping("/") // /주소가 요청되면 index 함수를 실행한다 (보통 홈은 기본으로 / 으로 표현된다)
+    public String index() {
+        return "index"; //=> templetes 폴더의 index.html 파일을 찾아감
+    }
+}
---
+++ src/main/java/com/example/dressing/dto/UserDTO.java
@@ -0,0 +1,29 @@
+//DTO: 클래스에 저장될 것들을 필드로 정리 (DB에 저장되는 것?)
+package com.example.dressing.dto;
+
+import com.example.dressing.entity.UserEntity;
+import lombok.*;
+
+//lombok 라이브러리가 제공해주는 어노테이션
+@Getter //getter 자동 만들어줌
+@Setter
+@NoArgsConstructor //기본 생성자
+@AllArgsConstructor //모든 변수를 포함한 생성자
+@ToString //toString 자동 생성
+public class UserDTO {
+    //필드는 모두 private
+    private Long id;
+    private String userName;
+    private String userId;
+    private String userPassword;
+
+    //UserEntity -> UserDTO 변환
+    public static UserDTO toUserDTO(UserEntity userEntity) {
+        UserDTO userDTO = new UserDTO();
+        userDTO.setId(userEntity.getId());
+        userDTO.setUserName(userEntity.getUserName());
+        userDTO.setUserId(userEntity.getUserId());
+        userDTO.setUserPassword(userEntity.getUserPassword());
+        return userDTO;
+    }
+}
