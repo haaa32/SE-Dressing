fa21dab5bb299c439d7c904510b9843fc54414bb
조하은 <nancy323@naver.com>
2023-11-09 00:50:55 +0900

    사진업로드 거의 다해감

--- src/main/java/com/example/dressing/controller/ClosetController.java
+++ src/main/java/com/example/dressing/controller/ClosetController.java
@@ -1,24 +1,35 @@
 package com.example.dressing.controller;
 
-import com.example.dressing.dto.ClosetDTO;
 import com.example.dressing.service.ClosetService;
 import lombok.RequiredArgsConstructor;
+import lombok.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
-@Controller
+import java.io.IOException;
+import java.util.List;
+
 @RequiredArgsConstructor
+@Controller
 public class ClosetController {
+
     private final ClosetService closetService;
-    @GetMapping("/user/main")
-    public String saveFile() {
-        return "main";
+
+    @GetMapping("/upload")
+    public String testUploadForm() {
+        return "upload";
     }
 
-    @PostMapping("/user/main")
-    public String save(@ModelAttribute ClosetDTO closetDTO)
-    {
-        closetService.save(closetDTO);
-        return "main";
+    @PostMapping("/upload")
+    public String uploadFile(@RequestParam("file") MultipartFile file, @RequestParam("files") List<MultipartFile> files) throws IOException {
+        closetService.saveFile(file);
+
+        for (MultipartFile multipartFile : files) {
+            closetService.saveFile(multipartFile);
+        }
+
+        return "redirect:/"; // redirect:/
     }
+
 }
