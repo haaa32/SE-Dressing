a1410fdb548a59ee880593e322b893f5f0319efc
조하은 <nancy323@naver.com>
2023-11-12 17:07:41 +0900

    이미지 저장 경로 변경

--- src/main/java/com/example/dressing/service/ClosetService.java
+++ src/main/java/com/example/dressing/service/ClosetService.java
@@ -4,12 +4,15 @@ import com.example.dressing.entity.ClosetEntity;
 import com.example.dressing.repository.ClosetRepository;
 
 import lombok.RequiredArgsConstructor;
-import lombok.Value;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.UUID;
 
 @RequiredArgsConstructor
@@ -17,6 +20,9 @@ import java.util.UUID;
 public class ClosetService {
 
     private final ClosetRepository closetRepository;
+
+    @Value("${file.upload-dir}")
+    private String uploadDir;
 
     public Long saveFile(MultipartFile files) throws IOException {
         if (files.isEmpty()) {
@@ -26,31 +32,30 @@ public class ClosetService {
         // 원래 파일 이름 추출
         String origName = files.getOriginalFilename();
 
-        // 파일 이름으로 쓸 uuid 생성
+        // 파일 이름으로 쓸 UUID 생성
         String uuid = UUID.randomUUID().toString();
 
-        // 확장자 추출(ex : .png)
+        // 확장자 추출 (예: .png)
         String extension = origName.substring(origName.lastIndexOf("."));
 
         // uuid와 확장자 결합
         String savedName = uuid + extension;
 
         // 파일을 불러올 때 사용할 파일 경로
-        String savedPath = "C:/Img_SW/" + savedName;
+        String savedPath = "static/images/" + savedName;
 
-        // 파일 엔티티 생성
-        ClosetEntity file = ClosetEntity.builder()
+        // 실제 파일이 저장될 경로 설정
+        Path path = Paths.get(uploadDir + File.separator + savedName);
+        Files.createDirectories(path.getParent());
+        Files.copy(files.getInputStream(), path);
+
+        ClosetEntity entity = ClosetEntity.builder()
                 .orgNm(origName)
                 .savedNm(savedName)
-                .savedPath(savedPath)
+                .savedPath("static/images/" + savedName)
                 .build();
 
-        // 실제로 로컬에 uuid를 파일명으로 저장
-        files.transferTo(new File(savedPath));
-
-        // 데이터베이스에 파일 정보 저장
-        ClosetEntity savedFile = closetRepository.save(file);
-
+        ClosetEntity savedFile = closetRepository.save(entity);
         return savedFile.getId();
     }
 }
