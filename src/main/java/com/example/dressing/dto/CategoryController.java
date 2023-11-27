d66713bc7c23ee09d93cdb50217a4b3690172b8b
조하은 <nancy323@naver.com>
2023-11-28 02:17:42 +0900

    test

--- src/main/java/com/example/dressing/dto/CategoryController.java
+++ src/main/java/com/example/dressing/dto/CategoryController.java
@@ -1,4 +1,4 @@
-package com.example.dressing.controller;
+package com.example.dressing.dto;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
